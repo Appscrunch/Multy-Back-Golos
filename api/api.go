8686a5f618e36e52e8067c64aacb9a7fa5787e39
Vladimir Evgrafov <evgrafov.vladimir@gmail.com>
2018-03-16 01:37:54 +0300

    MUL-967: support all balance changing operations
    Support all balance changing operations, provided by golos-go & fetch all relevant account names in it.
    Start fetching blocks from the current head

--- api/api.go
+++ api/api.go
@@ -167,6 +167,9 @@ func (api *API) SendTransaction(trx *types.Transaction) (*json.RawMessage, error
 	return api.client.NetworkBroadcast.BroadcastTransactionSynchronousRaw(trx)
 }
 
+// NewBlockLoop checks for new blocks and send them to chans
+// start is a number of starting block for iteration
+// if start is 0, using head_block_number
 func (api *API) NewBlockLoop(blockChan chan<- *NewBlockMessage, balanceChan chan<- *BalancesChangedMessage, done <-chan bool, start uint32) {
 	blockNum := start
 
@@ -182,6 +185,9 @@ func (api *API) NewBlockLoop(blockChan chan<- *NewBlockMessage, balanceChan chan
 			log.Printf("get global properties: %s", err)
 			time.Sleep(time.Duration(config.SteemitBlockInterval) * time.Second)
 			continue
+		}
+		if blockNum == 0 {
+			blockNum = props.HeadBlockNumber
 		}
 		// maybe LastIrreversibleBlockNum, cause possible microforks
 		if props.HeadBlockNumber-blockNum > 0 {
@@ -213,6 +219,79 @@ func (api *API) NewBlockLoop(blockChan chan<- *NewBlockMessage, balanceChan chan
 	}
 }
 
+// getNames gets account names of balance changing operations
+// based on https://developers.golos.io/golos-v0.17.0/da/d67/structgolos_1_1protocol_1_1base__operation.html
+func getNames(rawOp types.Operation) []string {
+	switch op := rawOp.Data().(type) {
+	case *types.VoteOperation: // vote_operation
+		return []string{op.Voter, op.Author}
+	case *types.TransferOperation: // transfer_operation
+		return []string{op.From, op.To}
+	case *types.TransferToVestingOperation: // transfer_to_vesting_operation
+		return []string{op.From, op.To}
+	case *types.WithdrawVestingOperation: // withdraw_vesting_operation
+		return []string{op.Account}
+	case *types.LimitOrderCreateOperation: // limit_order_create_operation
+		return []string{op.Owner}
+	case *types.LimitOrderCancelOperation: // limit_order_cancel_operation
+		return []string{op.Owner}
+	case *types.ConvertOperation: // convert_operation
+		return []string{op.Owner}
+	case *types.AccountCreateOperation: // account_create_operation
+		return []string{op.Creator}
+	case *types.WitnessUpdateOperation: // witness_update_operation
+		return []string{op.Owner}
+	case *types.POWOperation: // pow_operation
+		return []string{op.WorkerAccount}
+	case *types.SetWithdrawVestingRouteOperation: // set_withdraw_vesting_route_operation
+		return []string{op.FromAccount, op.ToAccount}
+	case *types.LimitOrderCreate2Operation: // limit_order_create2_operation
+		return []string{op.Qwner} // TODO: fix typo in golos-go
+	case *types.EscrowTransferOperation: // escrow_transfer_operation
+		return []string{op.From}
+	case *types.EscrowReleaseOperation: // escrow_release_operation
+		return []string{op.From, op.To, op.Agent}
+	case *types.POW2Operation: // pow2_operation
+		if op.Input != nil {
+			return []string{op.Input.WorkerAccount}
+		}
+		return []string{}
+	case *types.TransferToSavingsOperation: // transfer_to_savings_operation
+		return []string{op.From, op.To}
+	case *types.TransferFromSavingsOperation: // transfer_from_savings_operation
+		return []string{op.From, op.To}
+	case *types.ClaimRewardBalanceOperation: // claim_reward_balance_operation
+		return []string{op.Account}
+	case *types.DelegateVestingSharesOperation: // delegate_vesting_shares_operation
+		return []string{op.Delegatee, op.Delegator}
+	case *types.AccountCreateWithDelegationOperation: // account_create_with_delegation_operation
+		return []string{op.Creator, op.NewAccountName}
+	case *types.FillConvertRequestOperation: // fill_convert_request_operation
+		return []string{op.Owner}
+	case *types.AuthorRewardOperation: // author_reward_operation
+		return []string{op.Author}
+	case *types.CurationRewardOperation: // curation_reward_operation
+		return []string{op.CommentAuthor, op.Curator}
+	case *types.CommentRewardOperation: // comment_reward_operation
+		return []string{op.Author}
+	case *types.LiquidityRewardOperation: // liquidity_reward_operation
+		return []string{op.Owner}
+	case *types.InterestOperation: // interest_operation
+		return []string{op.Owner}
+	case *types.FillVestingWithdrawOperation: // fill_vesting_withdraw_operation
+		return []string{op.FromAccount, op.ToAccount}
+	case *types.FillOrderOperation: // fill_order_operation
+		return []string{op.CurrentOwner, op.OpenOwner}
+	case *types.FillTransferFromSavingsOperation: // fill_transfer_from_savings_operation
+		return []string{op.From, op.To}
+	case *types.ReturnVestingDelegationOperation: // return_vesting_delegation_operation
+		return []string{op.Account}
+	case *types.CommentBenefactorRewardOperation: // comment_benefactor_reward_operation
+		return []string{op.Author, op.Benefactor}
+	}
+	return nil
+}
+
 // processBalance finds ops that changes balance that involves tracked addresses
 // and pushes updated balances to chanel
 func (api *API) processBalance(block *database.Block, balanceChan chan<- *BalancesChangedMessage, done <-chan bool) {
@@ -221,12 +300,7 @@ func (api *API) processBalance(block *database.Block, balanceChan chan<- *Balanc
 	addrs := []string{}
 	for _, tx := range block.Transactions {
 		for _, op := range tx.Operations {
-			switch op.Type() {
-			// all the balance changing ops
-			case types.TypeTransfer:
-				typedOp := op.Data().(types.TransferOperation)
-				addrs = append(addrs, typedOp.From, typedOp.To)
-			}
+			addrs = append(addrs, getNames(op)...)
 		}
 	}
 	for _, addr := range addrs {
